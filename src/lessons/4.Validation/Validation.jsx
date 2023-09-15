93ebda686df3729fbe2aae0adae7e830cf65e4f8
Tresor-ilunga <19im065@esisalama.org>
2023-09-15 12:48:20 +0200

    Creating the conditions for validation

--- src/lessons/4.Validation/Validation.jsx
+++ src/lessons/4.Validation/Validation.jsx
@@ -15,9 +15,46 @@ export default function Validation() {
         password: false,
         passwordConfirmation: false
     })
-    console.log(inputsStates);
     function handleSubmit(e){
         e.preventDefault()
+
+        if(validationCheck()){
+            console.log("Envoi du formulaire");
+        }
+
+        function validationCheck(){
+            const areValid = {
+                pseudo: false,
+                password: false,
+                passwordConfirmation: false
+            }
+
+            if(inputsStates.pseudo.length < 3 || inputsStates.pseudo.length > 64){
+                setShowValidation(state => ({...state, pseudo: true}))
+            }
+            else {
+                areValid.pseudo = true
+                setShowValidation(state => ({...state, pseudo: false}))
+            }
+
+            if(inputsStates.password.length < 6 || !/\d/.test(inputsStates.password)){
+                setShowValidation(state => ({...state, password: true}))
+            }
+            else {
+                areValid.password = true
+                setShowValidation(state => ({...state, password: false}))
+            }
+
+            if(inputsStates.passwordConfirmation !== inputsStates.password){
+                setShowValidation(state => ({...state, passwordConfirmation: true}))
+            }
+            else {
+                areValid.passwordConfirmation = true
+                setShowValidation(state => ({...state, passwordConfirmation: false}))
+            }
+
+            console.log(areValid);
+        }
     }
     return (
         <form
